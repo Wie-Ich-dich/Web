bc779b64eb2811f88fd211995bdb618c52599e82
Chen Wj <1364146012@qq.com>
2022-08-06 17:21:12 +0800

    Program 4 revise 20220806

--- 4_Hidden Search/script.js
+++ 4_Hidden Search/script.js
@@ -3,12 +3,16 @@
 */
 const btn=document.getElementById('btn');
 const search=document.querySelector('.search');
+const input=document.querySelector('.input');
 
 btn.addEventListener('click',()=>{
-    if(search.classList.contains('active')){
-        console.log('正在搜索......');
-        search.classList.remove('active');
-    }else{
-        search.classList.add('active');
-    }
+    // if(search.classList.contains('active')){
+    //     console.log('正在搜索......');
+    //     search.classList.remove('active');
+    // }else{
+    //     search.classList.add('active');
+    // }
+    // 以上代码可用下面一行代替
+    search.classList.toggle('active');
+    input.focus();
 })
