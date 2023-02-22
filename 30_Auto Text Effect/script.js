d6c3e588af1d987f6d1eb4a967a35a3ddc0dfa55
Chen Wj <1364146012@qq.com>
2023-02-22 20:29:39 +0800

    20230222 Program30 final

---
+++ 30_Auto Text Effect/script.js
@@ -0,0 +1,28 @@
+const textEl=document.getElementById('text');
+const speedEl=document.getElementById('speed');
+const inputText=document.getElementById('inputText');
+
+inputText.value='';
+speedEl.value=1;
+let text="We love Programming!";
+let idx=1;
+let speed=300/speedEl.value;
+
+writeText();
+
+function writeText(){
+    // console.log('ok');
+    textEl.innerText=text.slice(0,idx);
+    idx++;
+    if(idx>text.length){
+        idx=1;
+    }
+    // 采用递归的形式实现循环显示
+    setTimeout(writeText,speed);
+}
+
+//这里如果使用循环函数则后面的改速度事件就触发不了了
+// setInterval(writeText,speed);
+
+speedEl.addEventListener('input',(e)=>{speed=300/e.target.value;})
+inputText.addEventListener('keydown',(e)=>{if(e.key==='Enter'){text=e.target.value;e.target.value='';}})
